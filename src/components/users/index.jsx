547ccd93cab1902bc5289186258ac4e7dfb95c20
Bruno <brunopetinati@hotmail.com>
2023-03-16 14:49:08 -0300

    refatorando o código, funções do componente users_table alteradas para users

--- src/components/users/index.jsx
+++ src/components/users/index.jsx
@@ -1,19 +1,90 @@
 import UsersTable from "../users_table/index";
 import DefaultButton from "../default_button";
-import { useSelector } from "react-redux";
 import { Container, TitleContainer, ButtonContainer, TableContainer } from "./styles";
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 const Users = () => {
 
+  const app_status = useSelector((state) => state.app_status.status);
   const collapsed = useSelector((state) => state.sidebar.status);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const token = sessionStorage.getItem('Authorization');
+        const response = await axios.get('http://localhost:8000/api/users/', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setUsers(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUsers();
+  }, [app_status]);
+
+
+
+  const [selectedColumn, setSelectedColumn] = useState('');
+  const [searchValue, setSearchValue] = useState('');
+
+
+  // Filter users based on the selected column and search value
+  const filteredUsers = users.filter((user) => {
+    if (!searchValue) {
+      return true;
+    }
+    if (selectedColumn === 'Nome' && user.full_name.toLowerCase().includes(searchValue.toLowerCase())) {
+      return true;
+    }
+    if (selectedColumn === 'Email' && user.email.toLowerCase().includes(searchValue.toLowerCase())) {
+      return true;
+    }
+    if (selectedColumn === 'Whatsapp' && user.phone.toLowerCase().includes(searchValue.toLowerCase())) {
+      return true;
+    }
+    if (selectedColumn === 'Localidade' && `${user.city}, ${user.state}`.toLowerCase().includes(searchValue.toLowerCase())) {
+      return true;
+    }
+    if (selectedColumn === 'Acesso' && user.user_type.toLowerCase().includes(searchValue.toLowerCase())) {
+      return true;
+    }
+    return false;
+  });
+
+  const handleColumnChange = (event) => {
+    setSelectedColumn(event.target.value);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchValue(event.target.value);
+  };
   
   return (
     <Container collapsed={collapsed} >
       <ButtonContainer>
+      <div>
+        <label htmlFor="column-select">Select a column to search:</label>
+        <select id="column-select" onChange={handleColumnChange}>
+          <option value="">---</option>
+          <option value="Nome">Nome</option>
+          <option value="Email">Email</option>
+          <option value="Whatsapp">Whatsapp</option>
+          <option value="Localidade">Localidade</option>
+          <option value="Acesso">Acesso</option>
+        </select>
+        <input type="text" placeholder="Search..." value={searchValue} onChange={handleSearchChange} />
+      </div>
         <DefaultButton text={'Adicionar Usuário'} path={'/intern_client_register'}/>
       </ButtonContainer>
       <TableContainer>
-        <UsersTable />
+        <UsersTable filteredUsers={filteredUsers} />
       </TableContainer>
     </Container>
   );
--- src/components/users_table/index.jsx
+++ src/components/users_table/index.jsx
@@ -1,78 +1,11 @@
 import { Table, THead, TR, TH, TD, Wrapper} from './styles'
-import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import axios from 'axios';
 
-const UsersTable = () => {
-  const [users, setUsers] = useState([]);
-  const [selectedColumn, setSelectedColumn] = useState('');
-  const [searchValue, setSearchValue] = useState('');
 
-  const app_status = useSelector((state) => state.app_status);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const token = sessionStorage.getItem('Authorization');
-        const response = await axios.get('http://localhost:8000/api/users/', {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        setUsers(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchUsers();
-  }, [app_status]);
-
-  // Filter users based on the selected column and search value
-  const filteredUsers = users.filter((user) => {
-    if (!searchValue) {
-      return true;
-    }
-    if (selectedColumn === 'Nome' && user.full_name.toLowerCase().includes(searchValue.toLowerCase())) {
-      return true;
-    }
-    if (selectedColumn === 'Email' && user.email.toLowerCase().includes(searchValue.toLowerCase())) {
-      return true;
-    }
-    if (selectedColumn === 'Whatsapp' && user.phone.toLowerCase().includes(searchValue.toLowerCase())) {
-      return true;
-    }
-    if (selectedColumn === 'Localidade' && `${user.city}, ${user.state}`.toLowerCase().includes(searchValue.toLowerCase())) {
-      return true;
-    }
-    if (selectedColumn === 'Acesso' && user.user_type.toLowerCase().includes(searchValue.toLowerCase())) {
-      return true;
-    }
-    return false;
-  });
-
-  const handleColumnChange = (event) => {
-    setSelectedColumn(event.target.value);
-  };
-
-  const handleSearchChange = (event) => {
-    setSearchValue(event.target.value);
-  };
+const UsersTable = ({filteredUsers}) => {
 
   return (
     <Wrapper>
-      <div>
-        <label htmlFor="column-select">Select a column to search:</label>
-        <select id="column-select" onChange={handleColumnChange}>
-          <option value="">---</option>
-          <option value="Nome">Nome</option>
-          <option value="Email">Email</option>
-          <option value="Whatsapp">Whatsapp</option>
-          <option value="Localidade">Localidade</option>
-          <option value="Acesso">Acesso</option>
-        </select>
-        <input type="text" placeholder="Search..." value={searchValue} onChange={handleSearchChange} />
-      </div>
+      
       <Table>
         <THead>
           <TR>
