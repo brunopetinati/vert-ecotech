936e5f3e6ff2a6d9e77705929f723997e2c3bd71
Bruno <brunopetinati@hotmail.com>
2023-06-01 18:15:56 -0300

    correção rápida nos gráficos

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -30,13 +30,13 @@ const StockChart = () => {
   ];
 
   const data3 = [
-    { name: "Jan", ativos: 377, aposentados: 290 },
-    { name: "Feb", ativos: 600, aposentados: 560 },
-    { name: "Mar", ativos: 800, aposentados: 400 },
-    { name: "Apr", ativos: 1230, aposentados: 900 },
-    { name: "May", ativos: 1890, aposentados: 4800 },
-    { name: "Jun", ativos: 2390, aposentados: 3800 },
-    { name: "Jul", ativos: 3490, aposentados: 4300 },
+    { name: "Out", ativos: 377, aposentados: 290 },
+    { name: "Nov", ativos: 600, aposentados: 560 },
+    { name: "Dez", ativos: 800, aposentados: 400 },
+    { name: "Jan", ativos: 1230, aposentados: 900 },
+    { name: "Fev", ativos: 1890, aposentados: 4800 },
+    { name: "Mar", ativos: 2390, aposentados: 3800 },
+    { name: "Abr", ativos: 3490, aposentados: 4300 },
   ];
 
   const data4 = [
@@ -45,13 +45,13 @@ const StockChart = () => {
     { name: "Dez", started: 3, analysis: 2, viability: 1, negotiation: 0, implementing: 0, concluded: 0 },
     { name: "Jan", started: 1, analysis: 0, viability: 5, negotiation: 0, implementing: 0, concluded: 0 },
     { name: "Fev", started: 5, analysis: 0, viability: 0, negotiation: 6, implementing: 0, concluded: 0 },
-    { name: "Mar", started: 11, analysis: 5, viability: 0, negotiation: 0, implementing: 6, concluded: 0 },
+    { name: "Mar", started: 4, analysis: 5, viability: 0, negotiation: 0, implementing: 6, concluded: 0 },
     { name: "Abr", started: 7, analysis: 2, viability: 3, negotiation: 0, implementing: 0, concluded: 6 },
   ];
   return (
     <Container>
       <Graph data={data}  title={'Acessos'} name={'name'} key_a={'pv'} key_b={'uv'} key_c={'amt'} stroke_a={'#8884d8'} stroke_b={'#82ca9d'} stroke_c={'brown'} />
-      <Graph data={data2} title={'Sequestro de CO2 na atmosfera'} name={'name'} key_a={'seq'} key_b={'emit'} key_c={'acm'} stroke_a={'#7eff00'} stroke_b={'#054d00'} stroke_c={'orange'}/>
+      <Graph data={data2} title={'Sequestro de Carbono na Atmosfera'} name={'name'} key_a={'seq'} key_b={'emit'} key_c={'acm'} stroke_a={'#7eff00'} stroke_b={'#054d00'} stroke_c={'orange'}/>
       <Graph data={data3} title={'Tokens'} name={'name'} key_a={'ativos'} key_b={'aposentados'} stroke_a={'#7eff00'} stroke_b={'black'} />
       <Graph data={data4} title={'Projetos'} name={'name'} key_a={'started'} key_b={'analysis'} key_c={'viability'} key_d={'negotiation'} key_e={'idle'} key_f={'implementing'} key_g={'concluded'} stroke_a={'blue'} stroke_b={'violet'} stroke_c={'green'} stroke_d={'gold'} stroke_e={'grey'} stroke_f={'orange'} stroke_g={'brown'}/>
     </Container>
