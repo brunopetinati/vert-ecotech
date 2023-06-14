f909dc176afcfd2d82c1e3282d6232fb6b69a7b7
Bruno <brunopetinati@hotmail.com>
2023-06-14 12:12:04 -0300

    zerando os gráficos

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -8,10 +8,10 @@ const StockChart = () => {
   //const [data, setData] = useState(null);
 
   const data = [
-    { name: "Jan", uv: 4000, pv: 2400, amt: 2400 },
-    { name: "Feb", uv: 3000, pv: 1398, amt: 2210 },
-    { name: "Mar", uv: 2000, pv: 8800, amt: 2290 },
-    { name: "Apr", uv: 2780, pv: 3908, amt: 2000 },
+    { name: "Jan", uv: 0, pv: 0, amt: 0 },
+    { name: "Feb", uv: 0, pv: 0, amt: 0 },
+    { name: "Mar", uv: 0, pv: 0, amt: 0 },
+    { name: "Apr", uv: 0, pv: 0, amt: 0 },
   ];
 
   // uv: short for "unique visitors", refers to the number of distinct individuals who visited a website or other online platform during a given time period.
@@ -20,33 +20,33 @@ const StockChart = () => {
 
 
   const data2 = [
-    { name: "Out", seq: 4000, emit: 2400, acm: 1600 },
-    { name: "Nov", seq: 3000, emit: 1398, acm: 1602 },
-    { name: "Dez", seq: 2000, emit: 9800, acm: -7800 },
-    { name: "Jan", seq: 2780, emit: 3908, acm: -1128 },
-    { name: "Fev", seq: 1890, emit: 4800, acm: -2910 },
-    { name: "Mar", seq: 2390, emit: 3800, acm:  -1410},
-    { name: "Abr", seq: 3490, emit: 4300, acm: -810 },
+    { name: "Out", seq: 0, emit: 0, acm: 0 },
+    { name: "Nov", seq: 0, emit: 0, acm: 0 },
+    { name: "Dez", seq: 0, emit: 0, acm: 0 },
+    { name: "Jan", seq: 0, emit: 0, acm: 0 },
+    { name: "Fev", seq: 0, emit: 0, acm: 0 },
+    { name: "Mar", seq: 0, emit: 0, acm: 0 },
+    { name: "Abr", seq: 0, emit: 0, acm: 0 },
   ];
 
   const data3 = [
-    { name: "Out", ativos: 377, aposentados: 290 },
-    { name: "Nov", ativos: 600, aposentados: 560 },
-    { name: "Dez", ativos: 800, aposentados: 400 },
-    { name: "Jan", ativos: 1230, aposentados: 900 },
-    { name: "Fev", ativos: 1890, aposentados: 4800 },
-    { name: "Mar", ativos: 2390, aposentados: 3800 },
-    { name: "Abr", ativos: 3490, aposentados: 4300 },
+    { name: "Out", ativos: 0, aposentados: 0 },
+    { name: "Nov", ativos: 0, aposentados: 0 },
+    { name: "Dez", ativos: 0, aposentados: 0 },
+    { name: "Jan", ativos: 0, aposentados: 0 },
+    { name: "Fev", ativos: 0, aposentados: 0 },
+    { name: "Mar", ativos: 0, aposentados: 0 },
+    { name: "Abr", ativos: 0, aposentados: 0 },
   ];
 
   const data4 = [
-    { name: "Out", started: 1, analysis: 0, viability: 0, negotiation: 0, implementing: 0, concluded: 0 },
-    { name: "Nov", started: 2, analysis: 1, viability: 0, negotiation: 0, implementing: 0, concluded: 0 },
-    { name: "Dez", started: 3, analysis: 2, viability: 1, negotiation: 0, implementing: 0, concluded: 0 },
-    { name: "Jan", started: 1, analysis: 0, viability: 5, negotiation: 0, implementing: 0, concluded: 0 },
-    { name: "Fev", started: 5, analysis: 0, viability: 0, negotiation: 6, implementing: 0, concluded: 0 },
-    { name: "Mar", started: 4, analysis: 5, viability: 0, negotiation: 0, implementing: 6, concluded: 0 },
-    { name: "Abr", started: 7, analysis: 2, viability: 3, negotiation: 0, implementing: 0, concluded: 6 },
+    { name: "Out", started: 0, analysis: 0, viability: 0, negotiation: 0, implementing: 0, concluded: 0 },
+    { name: "Nov", started: 0, analysis: 0, viability: 0, negotiation: 0, implementing: 0, concluded: 0 },
+    { name: "Dez", started: 0, analysis: 0, viability: 0, negotiation: 0, implementing: 0, concluded: 0 },
+    { name: "Jan", started: 0, analysis: 0, viability: 0, negotiation: 0, implementing: 0, concluded: 0 },
+    { name: "Fev", started: 0, analysis: 0, viability: 0, negotiation: 0, implementing: 0, concluded: 0 },
+    { name: "Mar", started: 0, analysis: 0, viability: 0, negotiation: 0, implementing: 0, concluded: 0 },
+    { name: "Abr", started: 0, analysis: 0, viability: 0, negotiation: 0, implementing: 0, concluded: 0 },
   ];
   return (
     <Container>
