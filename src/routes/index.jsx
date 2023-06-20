b5a9f2524fe93b9b9f19624efbea4fe0c65a208d
Bruno <brunopetinati@hotmail.com>
2023-06-20 19:26:30 -0300

    retornando requisições por thunk

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -28,6 +28,10 @@ const AppRoutes = () => {
   const dispatch = useDispatch();
 
   const login = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(getOwners());
+  }, [login]);
   
   if (!login.accessToken && location.pathname !== '/'&&
     location.pathname !== '/register' &&
