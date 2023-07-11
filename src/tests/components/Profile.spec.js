ae05146eb8a5a8cc0ca2b3923d9109d3562d1bd6
Bruno <brunopetinati@hotmail.com>
2023-07-11 14:11:36 -0300

    primeiro teste profile concluído com êxito

--- src/tests/components/Profile.spec.js
+++ src/tests/components/Profile.spec.js
@@ -1,16 +1,18 @@
 import { render } from "@testing-library/react";
 import Profile from "../../components/profile";
 import { Provider } from "react-redux";
-import store from "../../store";
 
-describe('Profile', () => {
-  it('should render successfully', () => {
+import configureStore from 'redux-mock-store';
 
-    const user = {
+// Create a mock Redux store
+const mockStore = configureStore([]);
+const store = mockStore({
+  user: {
+    currentUser: {
       id: 1,
       full_name: 'Vert Desenvolvimento',
       rg: '47.859.555-4',
-      cpf: '392.382.555-46' ,
+      cpf: '392.382.555-46',
       phone: '19 9 9727-9784',
       email: 'suporte@vertecotech.com',
       user_type: 'ADM',
@@ -21,10 +23,18 @@ describe('Profile', () => {
       complement: '12A',
       district: 'Jardim Ipê',
       state: 'SP',
-      city: 'Paulínia'
-    }
+      city: 'Paulínia',
+    },
+  },
+});
 
-    const { container } = render(<Provider store={store}><Profile user={user}/></Provider>);
+describe('Profile', () => {
+  it('should render successfully', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <Profile />
+      </Provider>
+    );
     expect(container).toBeTruthy();
   });
 });
