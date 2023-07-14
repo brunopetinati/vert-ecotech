1a9f56e309bcd6c05dc86154a0870ac462c6e6ae
Bruno <brunopetinati@hotmail.com>
2023-07-14 19:09:59 -0300

    corrigindo arquivo global

--- src/constants/global.js
+++ src/constants/global.js
@@ -5,7 +5,7 @@ const awsUrl = 'https://api.vertecotech.com';
 const stage = 'http://18.216.226.85:8000';
 const localHost = 'http://localhost:8000';
 
-export const currentUrl = localHost;
+export const currentUrl = awsUrl;
 
 
 // atenção:
