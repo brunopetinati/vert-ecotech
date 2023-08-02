0bd2165b090a6bf0e8a0c22ac83a70ab21b8dcf4
Bruno <brunopetinati@hotmail.com>
2023-08-02 13:20:15 -0300

    Finalizado gráfico de sequestro de carbono na atmosfera?

--- src/components/dashboard/carbon.js
+++ src/components/dashboard/carbon.js
@@ -1,24 +1,37 @@
-const { parseISO, getMonth } = require('date-fns');
+const getMonthName = (monthNumber) => {
+  const months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
+  return months[monthNumber - 1];
+};
 
-export const getTotalAreaByMonth = (projects) => {
+export const calculateTotalAreaByMonth = (projects) => {
+  const totalsByMonth = {};
 
-  if (!projects) return;
+  projects.forEach((project) => {
+    const date = new Date(project.created_at);
+    const monthYear = `${getMonthName(date.getMonth() + 1)}`; 
+    if (!totalsByMonth[monthYear]) {
+      totalsByMonth[monthYear] = 0;
+    }
+    totalsByMonth[monthYear] += project.total_area;
+  });
 
-  const months = ["Out", "Nov", "Dez", "Jan", "Fev", "Mar", "Abr"];
-
-  const totalAreaByMonth = projects.reduce((acc, project) => {
-    const createdDate = parseISO(project.created_at);
-    const month = getMonth(createdDate);
-    acc[month] += project.total_area;
-    return acc;
-  }, Array(7).fill(0));
-
-  const result = months.map((month, index) => ({
-    name: month,
-    seq: totalAreaByMonth[index],
-  }));
+  const result = [];
+  for (const monthYear in totalsByMonth) {
+    result.push({ month: monthYear, hectares: totalsByMonth[monthYear] });
+  }
 
   return result;
-}
-
-
+};
--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Chart from '../chart';
 import { Container } from './styles';
-import { getTotalAreaByMonth } from './carbon';
+import { calculateTotalAreaByMonth } from './carbon';
 
 const Dashboard = () => {
 
   const projects = useSelector((state) => state.app_data.projects);
-  console.log('meeeu deus', projects);
+  const data2 = calculateTotalAreaByMonth(projects);
+
 
   const data = [
     { name: "Jan", uv: 0, pv: 0, amt: 0 },
@@ -19,17 +20,6 @@ const Dashboard = () => {
   // uv: short for "unique visitors", refers to the number of distinct individuals who visited a website or other online platform during a given time period.
   // pv: short for "page views", refers to the total number of times a webpage or other digital content was viewed or accessed during a given time period.
   // amt: short for "amount", refers to the total monetary value of transactions or other financial activity that occurred during a given time period.
-
-
-  const data2 = [
-    { name: "Out", seq: 0 },
-    { name: "Nov", seq: 0 },
-    { name: "Dez", seq: 0 },
-    { name: "Jan", seq: 0 },
-    { name: "Fev", seq: 0 },
-    { name: "Mar", seq: 0 },
-    { name: "Abr", seq: 0 }
-  ];
 
 
 
@@ -56,7 +46,7 @@ const Dashboard = () => {
   return (
     <Container>
       <Chart data={data}  title={'Acessos'} name={'name'} key_a={'pv'} key_b={'uv'} key_c={'amt'} stroke_a={'#8884d8'} stroke_b={'#82ca9d'} stroke_c={'brown'} />
-      <Chart data={data2} title={'Sequestro de Carbono na Atmosfera'} name={'name'} key_a={'seq'} key_b={'emit'} key_c={'acm'} stroke_a={'#7eff00'} stroke_b={'#054d00'} stroke_c={'orange'}/>
+      <Chart data={data2} title={'Sequestro de Carbono na Atmosfera'} name={'month'} key_a={'hectares'} stroke_a={'#7eff00'} />
       <Chart data={data3} title={'Tokens'} name={'name'} key_a={'ativos'} key_b={'aposentados'} stroke_a={'#7eff00'} stroke_b={'black'} />
       <Chart data={data4} title={'Projetos'} name={'name'} key_a={'started'} key_b={'analysis'} key_c={'viability'} key_d={'negotiation'} key_e={'idle'} key_f={'implementing'} key_g={'concluded'} stroke_a={'blue'} stroke_b={'violet'} stroke_c={'green'} stroke_d={'gold'} stroke_e={'grey'} stroke_f={'orange'} stroke_g={'brown'}/>
     </Container>
