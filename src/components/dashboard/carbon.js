1c2abf7fd8d5aa894c6d15ee629767602b286afa
Bruno Petinati <brunopetinati@hotmail.com>
2023-08-04 23:45:01 -0300

    dashboard grafico co2 atmosfera

--- src/components/dashboard/carbon.js
+++ src/components/dashboard/carbon.js
@@ -17,6 +17,7 @@ const getMonthName = (monthNumber) => {
 };
 
 export const calculateTotalAreaByMonth = (projects) => {
+  
   const totalsByMonth = {};
 
   projects.forEach((project) => {
@@ -32,8 +33,62 @@ export const calculateTotalAreaByMonth = (projects) => {
   const result = [];
   for (const monthYear in totalsByMonth) {
     cumulativeTotal += totalsByMonth[monthYear];
+    result.push({ month: monthYear, estimate_ha: cumulativeTotal });
+  }
+
+  return result;
+};
+
+export const calculateTotalRealAreaByMonth = (projects) => {
+  const filteredProjects = projects.filter((project) => project.status !== null);
+
+  const totalsByMonth = {};
+
+  filteredProjects.forEach((project) => {
+    const date = new Date(project.created_at);
+    const monthYear = `${getMonthName(date.getMonth() + 1)}`;
+    if (!totalsByMonth[monthYear]) {
+      totalsByMonth[monthYear] = 0;
+    }
+    totalsByMonth[monthYear] += project.total_area;
+  });
+
+  let cumulativeTotal = 0;
+  const result = [];
+  for (const monthYear in totalsByMonth) {
+    cumulativeTotal += totalsByMonth[monthYear];
     result.push({ month: monthYear, hectares: cumulativeTotal });
   }
+
+  return result;
+};
+
+export const mergeCarbonData = (carbon_1, carbon_2) => {
+  // Cria um objeto para armazenar os dados mesclados usando o mês como chave
+  const mergedData = {};
+
+  // Itera sobre o array carbon_1 e adiciona as informações no objeto mesclado
+  carbon_1.forEach((item) => {
+    const { month, estimate_ha } = item;
+    if (!mergedData[month]) {
+      mergedData[month] = { month, estimate_ha, hectares: null };
+    } else {
+      mergedData[month].estimate_ha += estimate_ha;
+    }
+  });
+
+  // Itera sobre o array carbon_2 e adiciona as informações no objeto mesclado
+  carbon_2.forEach((item) => {
+    const { month, hectares } = item;
+    if (!mergedData[month]) {
+      mergedData[month] = { month, estimate_ha: null, hectares };
+    } else {
+      mergedData[month].hectares = hectares;
+    }
+  });
+
+  // Converte o objeto mesclado em um array
+  const result = Object.values(mergedData);
 
   return result;
 };
--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -2,14 +2,20 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Chart from '../chart';
 import { Container } from './styles';
-import { calculateTotalAreaByMonth } from './carbon';
+import { calculateTotalAreaByMonth, calculateTotalRealAreaByMonth, mergeCarbonData } from './carbon';
 
 const Dashboard = () => {
 
   const projects = useSelector((state) => state.app_data.projects);
-  const data2 = calculateTotalAreaByMonth(projects);
+  const carbon_1 = calculateTotalAreaByMonth(projects);
+  const carbon_2 = calculateTotalRealAreaByMonth(projects);
 
-  console.log(data2)
+  console.log(carbon_1);
+  console.log(carbon_2);
+
+  let data2 = mergeCarbonData(carbon_1, carbon_2);
+
+  console.log(data2);
 
   const data = [
     { name: "Jan", uv: 0, pv: 0, amt: 0 },
@@ -21,8 +27,6 @@ const Dashboard = () => {
   // uv: short for "unique visitors", refers to the number of distinct individuals who visited a website or other online platform during a given time period.
   // pv: short for "page views", refers to the total number of times a webpage or other digital content was viewed or accessed during a given time period.
   // amt: short for "amount", refers to the total monetary value of transactions or other financial activity that occurred during a given time period.
-
-
 
   const data3 = [
     { name: "Out", ativos: 0, aposentados: 0 },
@@ -47,7 +51,7 @@ const Dashboard = () => {
   return (
     <Container>
       <Chart data={data}  title={'Acessos'} name={'name'} key_a={'pv'} key_b={'uv'} key_c={'amt'} stroke_a={'#8884d8'} stroke_b={'#82ca9d'} stroke_c={'brown'} />
-      <Chart data={data2} title={'Sequestro de Carbono na Atmosfera'} name={'month'} key_a={'hectares'} stroke_a={'#7eff00'} />
+      <Chart data={data2} title={'Sequestro de Carbono na Atmosfera (ha)'} name={'month'} key_a={'hectares'} stroke_a={'#7eff00'} key_b={'estimate_ha'} stroke_b={'#054d00'}/>
       <Chart data={data3} title={'Tokens'} name={'name'} key_a={'ativos'} key_b={'aposentados'} stroke_a={'#7eff00'} stroke_b={'black'} />
       <Chart data={data4} title={'Projetos'} name={'name'} key_a={'started'} key_b={'analysis'} key_c={'viability'} key_d={'negotiation'} key_e={'idle'} key_f={'implementing'} key_g={'concluded'} stroke_a={'blue'} stroke_b={'violet'} stroke_c={'green'} stroke_d={'gold'} stroke_e={'grey'} stroke_f={'orange'} stroke_g={'brown'}/>
     </Container>
