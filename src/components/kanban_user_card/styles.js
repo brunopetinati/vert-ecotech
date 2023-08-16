83778dd7f09c3e2f1b3263c21f83dda17ae64b09
Bruno Petinati <brunopetinati@hotmail.com>
2023-08-15 23:19:47 -0300

    alterando layout do kanban, limpando consoles.log

--- src/components/kanban_user_card/styles.js
+++ src/components/kanban_user_card/styles.js
@@ -8,8 +8,9 @@ export const Card = styled.div`
   padding: 8px;
   margin-bottom: 8px;
   cursor: pointer;
-  min-width: 250px;
-  max-width: 250px;
+  min-width: 110px;
+  max-width: 110px;
+  height: 110px;
 `;
 
 export const InfoContainer = styled.div`
@@ -25,15 +26,18 @@ export const DisplayColumn = styled.div`
   align-items: flex-start;
   justify-content: center;
   max-width: 130px;
+
+  h3 {
+    font-size: 12px;
+  }
 `;
 
 export const ImgContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
-  align-items: flex-end;
 `;
 
 export const Img = styled.img`
-  width: 64px;
+  width: 16px;
 `;
