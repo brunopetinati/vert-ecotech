a3e9c514b173c551db56ece94a1cb8894ea73f1b
m7julia <julia@localhost.localdomain>
2025-03-12 10:44:47 -0300

    menu ambiental fixed

--- src/components/project_tabs/index.jsx
+++ src/components/project_tabs/index.jsx
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { appStatus } from '../../store/modules/app_status/actions';
+import { appStatus } from "../../store/modules/app_status/actions";
 import { useNavigate, useLocation } from "react-router-dom";
 import Swal from "sweetalert2";
 import axios from "axios";
 import { currentUrl } from "../../constants/global";
-import { TabContainer, LinearTabs, Tab, Content} from "./styles";
-import { ButtonContainer, Button, StyledButtonEditar,StyledButtonVoltar } from '../../pages/project_intern/intel/styles.js';
+import { TabContainer, LinearTabs, Tab, Content } from "./styles";
+import {
+  ButtonContainer,
+  Button,
+  StyledButtonEditar,
+  StyledButtonVoltar,
+} from "../../pages/project_intern/intel/styles.js";
 
 const ProjectTabs = ({ tabs, handleRegister, project }) => {
   const currentUser = useSelector((state) => state.user.currentUser);
   const [activeTab, setActiveTab] = useState(0);
   const collapsed = useSelector((state) => state.sidebar);
   let status = project.status;
+
+  //const collapsed = useSelector((state) => state.sidebar.collapsed);
 
   // Check if status is null on component mount and whenever it changes
   useEffect(() => {
@@ -31,31 +38,38 @@ const ProjectTabs = ({ tabs, handleRegister, project }) => {
   const navigate = useNavigate();
 
   const startProject = () => {
-    const token = sessionStorage.getItem('Authorization');
-    const headers = { Authorization: `Bearer ${token}`, };
+    const token = sessionStorage.getItem("Authorization");
+    const headers = { Authorization: `Bearer ${token}` };
 
     axios
-    .post(`${currentUrl}/api/engineering/`, { 'project': project.id, 'status': 'started' }, { headers })
-    .then((response) => {
-    })
-    .catch((error) => {
-      console.error('registration failed!', error);          
-    });
-    
-    axios
-    .put(`${currentUrl}/api/projects/${project.id}/update/`, { status: 'started', owner: project.owner, 'title': project.title }, { headers } )
-    .then((response) => {
-      Swal.fire({
-        title: 'Sucesso!',
-        text: 'O projeto foi inicializado com sucesso!',
-        icon: 'success',
-        confirmButtonText: 'OK'
+      .post(
+        `${currentUrl}/api/engineering/`,
+        { project: project.id, status: "started" },
+        { headers }
+      )
+      .then((response) => {})
+      .catch((error) => {
+        console.error("registration failed!", error);
       });
-      navigate('/analysis_and_development');
-    })
-    .catch((error) => {
-      // Handle the error if any
-    });
+
+    axios
+      .put(
+        `${currentUrl}/api/projects/${project.id}/update/`,
+        { status: "started", owner: project.owner, title: project.title },
+        { headers }
+      )
+      .then((response) => {
+        Swal.fire({
+          title: "Sucesso!",
+          text: "O projeto foi inicializado com sucesso!",
+          icon: "success",
+          confirmButtonText: "OK",
+        });
+        navigate("/analysis_and_development");
+      })
+      .catch((error) => {
+        // Handle the error if any
+      });
   };
 
   // Rotas
@@ -64,24 +78,23 @@ const ProjectTabs = ({ tabs, handleRegister, project }) => {
   const user = location?.state?.user;
 
   const handleComeBack = () => {
-
-    if(user)
-    {
-      dispatch(appStatus('edit_user'));
-      navigate('/welcome', { state: { user }});
+    if (user) {
+      dispatch(appStatus("edit_user"));
+      navigate("/welcome", { state: { user } });
+    } else {
+      dispatch(appStatus("Desenvolvimento"));
+      navigate("/analysis_and_development");
     }
-    else
-    {
-      dispatch(appStatus('Desenvolvimento'));
-      navigate('/analysis_and_development');
-    }
+  };
 
+  LinearTabs.defaultProps = {
+    collapsed: true, // Valor padrão caso não receba do Redux
   };
 
   return (
     <div>
       <TabContainer collapsed={collapsed}>
-        <LinearTabs>
+        <LinearTabs collapsed={collapsed}>
           {tabs.map((tab, index) => (
             <Tab
               key={index}
@@ -93,20 +106,32 @@ const ProjectTabs = ({ tabs, handleRegister, project }) => {
             </Tab>
           ))}
         </LinearTabs>
-        <Content collapsed={collapsed}>
-          {tabs[activeTab].content}
-        </Content>
+        <Content collapsed={collapsed}>{tabs[activeTab].content}</Content>
       </TabContainer>
 
-       <div style={{ paddingBottom: '50px'}}>
-        {activeTab === 0 && 
+      <div style={{ paddingBottom: "50px" }}>
+        {activeTab === 0 && (
           <ButtonContainer>
-            <StyledButtonEditar onClick={handleRegister} style={{ margin: '0px 10px 0px 0px' }}>Editar Informações</StyledButtonEditar>
-            <StyledButtonVoltar onClick={() => handleComeBack()} style={{ margin: '0px 10px 0px 0px' }}>Voltar</StyledButtonVoltar>
-            {project.status === null && currentUser.user_type === "ADM" && <Button onClick={() => startProject()}>Inicializar Processo</Button>}
-          </ButtonContainer>}        
+            <StyledButtonEditar
+              onClick={handleRegister}
+              style={{ margin: "0px 10px 0px 0px" }}
+            >
+              Editar Informações
+            </StyledButtonEditar>
+            <StyledButtonVoltar
+              onClick={() => handleComeBack()}
+              style={{ margin: "0px 10px 0px 0px" }}
+            >
+              Voltar
+            </StyledButtonVoltar>
+            {project.status === null && currentUser.user_type === "ADM" && (
+              <Button onClick={() => startProject()}>
+                Inicializar Processo
+              </Button>
+            )}
+          </ButtonContainer>
+        )}
       </div>
-
     </div>
   );
 };
